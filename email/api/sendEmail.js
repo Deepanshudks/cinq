ea5f66414990b3c91f08ea8e4773977cb26588ca
MOJO REALTY <mojorealty555@gmail.com>
2026-09-21 23:46:14 +0530

    Update sendEmail.js

--- email/api/sendEmail.js
+++ email/api/sendEmail.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
   }
 
   try {
-    const { name, phone, email, message } = req.body;
+    const { name, phone, email, preferredDate } = req.body;
 
     const transporter = nodemailer.createTransport({
       service: "gmail",
